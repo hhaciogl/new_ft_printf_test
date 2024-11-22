2e0a34e4b720bdd7ed40b31e910d1d25cf2152c2
Hasan Hacıoğlu <hhaciogl@student.42istanbul.com.tr>
2024-11-22 12:23:06 +0300

    update

--- silbeni/original_printf.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-#include <string.h>
-
-void my_print(char *str)
-{
-    write(1,str, strlen(str));
-}
--- silbeni/original_printf.h
+++
@@ -1,6 +0,0 @@
-#ifndef ORIGINAL_PRINTF_H
-#define ORIGINAL_PRINTF_H
-
-void my_print(char *str);
-
-#endif
