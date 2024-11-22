1be2e99acb21967ccb55e75bfcdb5b3b121838a6
hhaciogl <hhaciogl@student.42istanbul.com.tr>
2024-11-23 02:24:54 +0300

    add second argument

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3,7 +3,7 @@
 
 extern char my_buff[1024];
 
-int test_ft_printf(char *line_no, const char *format, void *arg, char *exp_sout, int exp);
+int test_ft_printf(char *line_no, const char *format, void *arg, void *arg1, char *exp_sout, int exp);
 int prints_a_single_char(void);
 int prints_a_string(void);
 
@@ -25,32 +25,32 @@ void tests_ft_printf()
 int prints_a_single_char(void)
 {
     return (
-        test_ft_printf("28", "This is a char: %c", (void *)'z', "This is a char: z", 17) &&
-        test_ft_printf("29", "This is a char: %c", (void *)'%', "This is a char: %", 17) &&
-        test_ft_printf("30", "This is \0a char: %c", (void *)'%', "This is ", 8) &&
-        test_ft_printf("31", "[%c]", (void *)'\0', "[]", 3) &&
-        test_ft_printf("32", "[%c]", (void *)'\n', "[\n]", 3)
+        test_ft_printf("28", "This is a char: %c", (void *)'z', NULL, "This is a char: z", 17) &&
+        test_ft_printf("29", "This is a char: %c", (void *)'%', NULL, "This is a char: %", 17) &&
+        test_ft_printf("30", "This is \0a char: %c", (void *)'%', NULL, "This is ", 8) &&
+        test_ft_printf("31", "[%c]", (void *)'\0', NULL, "[]", 3) &&
+        test_ft_printf("32", "[%c]", (void *)'\n', NULL, "[\n]", 3)
     );
 }
 int prints_a_string(void)
 {
     return (
-        test_ft_printf("38", "This is a string: %s", (void *)"str", "This is a string: str", 21) &&
-        test_ft_printf("39", "This is a string: %s", (void *)"st\0r", "This is a string: st", 20) &&
-        test_ft_printf("40", "This is an empty string:%s", (void *)"", "This is an empty string:", 24) &&
+        test_ft_printf("38", "This is a string: %s", (void *)"str", NULL, "This is a string: str", 21) &&
+        test_ft_printf("39", "This is a string: %s", (void *)"st\0r", NULL, "This is a string: st", 20) &&
+        test_ft_printf("40", "This is an empty string:%s", (void *)"", NULL, "This is an empty string:", 24) &&
         1
     );
 }
 
 
-int test_ft_printf(char *line_no, const char *format, void *arg, char *exp_sout, int exp)
+int test_ft_printf(char *line_no, const char *format, void *arg, void *arg1, char *exp_sout, int exp)
 {
     char *act_sout;
     int act;
     int cond;
     bzero(my_buff, 1024);
     
-    act = ft_printf(format, arg);
+    act = ft_printf(format, arg, arg1);
     act_sout = my_buff;
     cond = strcmp(act_sout, exp_sout) == 0 && act == exp;
     if (cond == 0 || DEBUG)
